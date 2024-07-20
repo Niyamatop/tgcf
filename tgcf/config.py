e1c3673bd914db2a932e73d06a8892fe7933643e
Dark souls <87437573+Niyamatop@users.noreply.github.com>
2024-07-21 02:02:28 +0530

    Update config.py

--- tgcf/config.py
+++ tgcf/config.py
@@ -68,7 +68,7 @@ class LoginConfig(BaseModel):
     phone_no: int = 918573899092
     USERNAME: str = "DARKCURSED0"
     SESSION_STRING: str = "1BVtsOLwBu0m2A5RBTx7sK7sNK0kRT4YjQEjYLm5RCm1NrkU0_0-sO_4QEdJ2Rk2xdAYBPPtJjQHY8eLpA1qjfLaOVmblfMjP3QUOqV15v1qTmmQuANXArX31a1jbTlOUyWqywTVbqbHpnqN7Al4A0igxdJ7XA8Am64xahNof82sRokSlr1YPTuG9dzvj98lzW3QTqU-jWC0lBgu86Ok5NaYwfSQ_EHCS0qjRvpVTfepFh8Jt9XcbRKky8NqtrG2-UaaR_eHk_T7YPtcvRKJ7ynFq2LBGcyRBi9AROHHtaCzY4xpss33B73Wc5iyT0mspFKsFNw3ZbOEwI1o162gRcaIj6Rm3uxg="
-    BOT_TOKEN: str = "7187592356:AAEcLMZc_lLffrhpTUXjCSLeeSQUKXzsnyU"
+    BOT_TOKEN: str = "7346455769:AAG4S9buEgIlCfwT-Vk80PaODeILZ_hLWlw"
 
 
 class BotMessages(BaseModel):
